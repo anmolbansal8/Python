68ebefd48d56236c4444832dfbc8fbd2586395ea
anmolbansal8 <141552580+anmolbansal8@users.noreply.github.com>
2023-09-01 19:58:25 +0530

    Add files via upload

---
+++ literals.py
@@ -0,0 +1,69 @@
+#string literals
+# in single quote
+s = 'geekforgeeks'
+
+# in double quotes
+t = "geekforgeeks"
+
+# multi-line String
+m = '''geek
+		   for
+			    geeks'''
+
+print(s)
+print(t)
+print(m)
+
+
+#character literals
+a="b"
+c='d'
+print(a)
+print(c)
+
+#numerical literals
+
+# Binary Literals
+a = 0b100
+
+# Decimal Literal
+b = 50
+
+# Octal Literal
+c = 0o320
+
+# Hexadecimal Literal
+d = 0x12b
+
+print(a, b, c, d)
+
+#float literals
+# Float Literal
+e = 24.8
+f = 45.0
+
+print(e, f)
+
+#complex literals
+z = 7 + 5j
+
+# real part is 0 here.
+k = 7j
+
+print(z, k)
+
+#boolen literals
+a = (1 == True)
+b = (1 == False)
+c = True + 3
+d = False + 7
+
+print("a is", a)
+print("b is", b)
+print("c:", c)
+print("d:", d)
+
+#special literals
+x=None
+print(x)
+
