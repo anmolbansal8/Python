bb2a4794563bab113089157ecb65a05be43be87e
anmolbansal8 <141552580+anmolbansal8@users.noreply.github.com>
2023-09-03 17:01:34 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+# division (float)
+a=10
+b=3
+c=a/b
+print(c)
+
+#division (int)
+a=10
+b=3
+c=a//b
+print(c)
+
+#modulus
+a=10
+b=3
+c=a%b
+print(c)
+
+#power
+a=2
+b=5
+print(a**b)
+
+
+#not in
+a=10
+
+lst=[10,20,40,50]
+if(a in lst):
+  print("present")
+else:
+  print("not present")
+b=30
+if(b in lst):
+  print("present")
+else:
+  print("not present")
